305e78542dfed63bd21b4df4089b6da4f9e1d64f
Nishchay200 <22103284@mail.jiit.ac.in>
2024-08-10 10:38:45 +0530

    q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=2;i<=n;i++){
+            int l=0;
+        for(int j=1;j<=i;j++){
+            if(i%j==0){
+                l++;
+            }
+        }
+        if(l==2){
+            cout<<i<<" ";
+        }
+    }
+}
 
